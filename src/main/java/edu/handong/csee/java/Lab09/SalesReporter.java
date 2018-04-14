fed26bbce8abee655c689ecd64b0bc7f2bb10d5f
21700500Myeonghui <21700500@handong.edu>
2018-04-14 10:07:14 +0900

    coding

--- src/main/java/edu/handong/csee/java/Lab09/SalesReporter.java
+++ src/main/java/edu/handong/csee/java/Lab09/SalesReporter.java
@@ -1,6 +1,7 @@
 package edu.handong.csee.java.Lab09;//Instructs the SalesReporter class to be complied and stored in edu.handong.csee.java.lab09
 
 import java.util.Scanner;//importing a package, java.util.Scanner. A package is a kind of external objects created by other developer
+import java.util.ArrayList;
 
 /**
  * This class defines a SalesReporter object
@@ -13,20 +14,15 @@ import java.util.Scanner;//importing a package, java.util.Scanner. A package is 
 public class SalesReporter {
 	private double highestSales;//Create double type Instance variable highestSales
 	private double averageSales;//Create double type Instance variable averageSales
-	private SalesAssociate[] team;//Declare the reference variable team for SalesAssociate arrays
-	private int numOfSalesman;//Create integer type Instance variable numOfSalesman
-
+	private ArrayList<SalesAssociate> team= new ArrayList<SalesAssociate>();
+	
 	/**
 	 * This is constructor for initializing object
 	 * constructor without parameter
 	 */
 	public SalesReporter()
 	{
-		System.out.println("Enter number of Sales associates: ");//Outputs the string literal "Enter number of Sales associates: "
-
-		Scanner keyboard =new Scanner(System.in);//Create an object of the Scanner class
-		numOfSalesman=keyboard.nextInt();//Take one integer from the keyboard and place it the numOfSalesman
-
+		
 	}
 
 	/**
@@ -34,27 +30,32 @@ public class SalesReporter {
 	 */
 	public void getData()
 	{
-		team = new SalesAssociate[numOfSalesman];//Create reference array, The size of the array is the value of the instance variable numOfSalesman
 
-
-		for(int i=0; i<team.length; i++)//Create integer type local variable i, Put 0 in variable i, increase i by 1, Repeat until i is smaller than the size of the array team.
+		boolean getDT=true;
+		
+		Scanner keyboard= new Scanner(System.in);
+		
+		while(getDT==true)
 		{
-			Scanner keyboard =new Scanner(System.in);//Create an object of the Scanner class
-			System.out.println("Enter data for associate number "+(i+1));//Outputs "Enter data for associate number 'Value of variable i plus 1'"
-
 			System.out.print("Enter name of sales associate: ");//Outputs the string literal "Enter name of sales associate: "
 			String name= keyboard.nextLine();//Create String type local variable name, Take one string from the keyboard and place it in name
 
 			System.out.print("Enter associate's sales: $");//Outputs the string literal "Enter associate's sales: $"
 			double sales = keyboard.nextDouble();//Create double type local variable sales, Take one double from the keyboard and place it in sales
-
-			team[i]= new SalesAssociate();//Create each element object in the array
-
-			team[i].setName(name);//calling method setName(String name) in the i-th of array team
-			team[i].setSales(sales);//calling method setSales(double sales) in the i-th of array team
-
+		
+			SalesAssociate member=new SalesAssociate();
+			member.setName(name);
+			member.setSales(sales);
+			
+			team.add(member);
+			
+			System.out.println("More data for the team");
+			String answer=keyboard.nextLine();
+			
+			if(!answer.equalsIgnoreCase("yes"))
+				getDT=true;
 		}
-
+		
 	}
 
 	/**
@@ -62,14 +63,12 @@ public class SalesReporter {
 	 */
 	public void computeHighestSales()
 	{
-		highestSales= team[0].getSales();//put the return value of method getSales() in 0-th of array team in highestSales
-
-		for(int i=1; i<team.length; i++)//Create integer type local variable i, Put 1 in variable i, increase i by 1, Repeat until i is smaller than the size of the array team.
+		highestSales=team.get(0).getSales();
+		
+		for(SalesAssociate member: team)
 		{
-			if(highestSales<team[i].getSales())//if the value of the highestSales is less than the return value of method getSales() in i-th of array team 
-			{
-				highestSales=team[i].getSales();//put the return value of method getSales() in i-th of array team in highestSales
-			}
+			if(highestSales <member.getSales())
+				highestSales=member.getSales();
 		}
 	}
 
@@ -80,11 +79,12 @@ public class SalesReporter {
 	{
 		double sum=0;//Create double type local variable sum, put 0 in sum
 
-		for(int i=0; i<team.length; i++)//Create integer type local variable i, Put 0 in variable i, increase i by 1, Repeat until i is smaller than the size of the array team.
+		for(SalesAssociate member: team)
 		{
-			sum+=team[i].getSales();//put the value of sum plus the return value of method getSales() in i-th of array team in sum
+			sum+=member.getSales();
 		}
-		averageSales=sum/numOfSalesman;//Put the value of the sum divided by numOfSalesman in averageSales
+		averageSales=sum/team.size();
+		
 	}
 
 	/**
@@ -96,24 +96,24 @@ public class SalesReporter {
 		System.out.println("The highest sales figure is $"+highestSales);//Outputs "The highest sales figure is $ ' the value of Instance variable highestSales'"
 
 		System.out.println("The following had the highest sales:");//Outputs the string literal "The following had the highest sales:"
-		for(int i=0; i<team.length; i++)//Create integer type local variable i, Put 0 in variable i, increase i by 1, Repeat until i is smaller than the size of the array team.
+		for(SalesAssociate member:team)//Create integer type local variable i, Put 0 in variable i, increase i by 1, Repeat until i is smaller than the size of the array team.
 		{
-			if(highestSales == team[i].getSales())//if the value of highestSales and the return value of method getSales() in i-th of array team are the same
+			if(highestSales == member.getSales())//if the value of highestSales and the return value of method getSales() in i-th of array team are the same
 			{
-				System.out.println("Name: " + team[i].getName());//Outputs "Name: ' the return value of method getName() in i-th of array team'"
+				System.out.println("Name: " + member.getName());//Outputs "Name: ' the return value of method getName() in i-th of array team'"
 				System.out.println("Sales: $ "+highestSales);//Outputs "Sales: $ 'the value of Instance variable highestSales'"
-				differenceAverage(team[i].getSales());//calling method differenceAverage(double iSales), Send the return value of method getSales() in i-th of array team as value of the parameter at this time
+				differenceAverage(member.getSales());//calling method differenceAverage(double iSales), Send the return value of method getSales() in i-th of array team as value of the parameter at this time
 			}
 		}
 
 		System.out.println("The rest performed as follows: ");// Output the string literal "The rest performed as follows: "
-		for(int j=0; j<team.length ;j++)//Create integer type local variable j, Put 0 in variable j, increase j by 1, Repeat until j is smaller than the size of the array team.
+		for(SalesAssociate member2: team)//Create integer type local variable j, Put 0 in variable j, increase j by 1, Repeat until j is smaller than the size of the array team.
 		{
-			if(highestSales != team[j].getSales())//if the value of highestSales and the return value of method getSales() in j-th of array team are not the same
+			if(highestSales != member2.getSales())//if the value of highestSales and the return value of method getSales() in j-th of array team are not the same
 			{
-				System.out.println("Name: "+team[j].getName());//Outputs "Name: ' the return value of method getName() in j-th of array team'"
-				System.out.println("Sales: "+team[j].getSales());//Outputs "Sales: $ 'the return value of method getSales() in j-th of array team'"
-				differenceAverage(team[j].getSales());//calling method differenceAverage(double iSales), Send the return value of method getSales() in j-th of array team as value of the parameter at this time
+				System.out.println("Name: "+member2.getName());//Outputs "Name: ' the return value of method getName() in j-th of array team'"
+				System.out.println("Sales: "+member2.getSales());//Outputs "Sales: $ 'the return value of method getSales() in j-th of array team'"
+				differenceAverage(member2.getSales());//calling method differenceAverage(double iSales), Send the return value of method getSales() in j-th of array team as value of the parameter at this time
 			}
 		}
 	}
